2f4127b292f3f2dd033ada2d309b9b462cfa9e04
asabriye <abdullahisabriye@gmail.com>
2021-11-09 22:47:32 +0000

    longUrl

--- express_server.js
+++ express_server.js
@@ -25,6 +25,13 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL 
+  const longURL = urlDatabase[shortURL]
+  const templateVars = { shortURL, longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
